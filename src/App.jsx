47d0489b5eac7598b6e0c523792ebae703d7e72d
AvinashSirohi121 <avinashsirohi86@gmail.com>
2024-01-31 14:08:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Converter from "./components/Converter";
+
+function App() {
+  return (
+    <>
+     
+      
+          <Converter className="z-50" />
+        
+    
+    </>
+  );
+}
+
+export default App;
